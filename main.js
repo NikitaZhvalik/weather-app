df5295b87f856a5d3320d599e5f1c33e98559df4
Nikita Zhvalik <130430335+NikitaZhvalik@users.noreply.github.com>
2023-07-21 22:48:32 +1000

    v.03 - выполненена верхняя секция + вкладки

--- main.js
+++ main.js
@@ -9,3 +9,22 @@ const toggleSearchPanel = (e) => {
 }
 
 searchPanelContainer.addEventListener('click', toggleSearchPanel)
+
+const weatherInterval = document.querySelector('.weather-interval');
+const weatherIntervalBtnActive = document.querySelector('.weather-interval__btn_active');
+const weatherIntervalBtnNoActive = document.querySelector('.weather-interval__btn_no-active');
+const weatherIntervalWeek = document.querySelector('.weather-card-box_active');
+const weatherIntervalHours = document.querySelector('.weather-card-box_no-active');
+
+const toggleInterval = (e) => {
+    if (e.target.matches('.weather-interval__btn_no-active')) {
+        weatherIntervalBtnActive.classList.toggle('weather-interval__btn_no-active');
+        weatherIntervalBtnActive.classList.toggle('weather-interval__btn_active');
+        weatherIntervalBtnNoActive.classList.toggle('weather-interval__btn_active');
+        weatherIntervalBtnNoActive.classList.toggle('weather-interval__btn_no-active');
+        weatherIntervalHours.classList.toggle('none');
+        weatherIntervalWeek.classList.toggle('none');
+    }
+}
+
+weatherInterval.addEventListener('click', toggleInterval)
